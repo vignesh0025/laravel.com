29057a6550c66529a9a245c2ab421936ec0e9ba6
Vignesh D <vignesh0025@gmail.com>
2015-11-10 23:09:19 +0530

    Update api.sh

--- build/api.sh
+++ build/api.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-cd /home/forge/laravel.com/build/sami
+cd ..
+LHOME=$(pwd)
 
-rm -rf /home/forge/laravel.com/build/sami/build
-rm -rf /home/forge/laravel.com/build/sami/cache
+cd $LHOME/build/sami
+
+#if [ ! -d "$LHOME/public/api" ]; then
+#  mkdir $LHOME/public/api
+#fi
+
+rm -rf $LHOME/public/api
+mkdir $LHOME/public/api
+
+rm -rf $LHOME/build/sami/build
+rm -rf $LHOME/build/sami/cache
 
 # Run API Docs
-git clone https://github.com/laravel/framework.git /home/forge/laravel.com/build/sami/laravel
+git clone https://github.com/laravel/framework.git $LHOME/build/sami/laravel
 
-php /home/forge/laravel.com/vendor/bin/sami.php update /home/forge/laravel.com/build/sami/sami.php
+php $LHOME/vendor/bin/sami.php update $LHOME/build/sami/sami.php
 
-cp -r /home/forge/laravel.com/build/sami/build/* /home/forge/laravel.com/public/api
+cp -r $LHOME/build/sami/build/* $LHOME/public/api
 
-rm -rf /home/forge/laravel.com/build/sami/build
-rm -rf /home/forge/laravel.com/build/sami/cache
+rm -rf $LHOME/build/sami/build
+rm -rf $LHOME/build/sami/cache
 
 # Cleanup
-rm -rf /home/forge/laravel.com/build/sami/laravel
+rm -rf $LHOME/build/sami/laravel
