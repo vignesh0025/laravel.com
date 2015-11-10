6a36fb1a1d24cd57990812eab9e92df6363a1362
Vignesh D <vignesh0025@gmail.com>
2015-11-10 23:12:07 +0530

    Update docs.sh

--- build/docs.sh
+++ build/docs.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-cd /home/forge/laravel.com/resources/docs/5.0 && git pull origin 5.0
-cd /home/forge/laravel.com/resources/docs/5.1 && git pull origin 5.1
-cd /home/forge/laravel.com/resources/docs/master && git pull origin master
+
+cd ..
+LHOME=$(pwd)
+
+rm -rf $LHOME/resources/docs
+mkdir $LHOME/resources/docs
+
+mkdir $LHOME/resources/docs/5.0
+cd $LHOME/resources/docs/5.0 && git pull origin 5.0
+
+mkdir $LHOME/resources/docs/5.1
+cd $LHOME/resources/docs/5.1 && git pull origin 5.1
+
+mkdir $LHOME/resources/docs/master
+cd $LHOME/resources/docs/master && git pull origin master
